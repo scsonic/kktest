f8863d08e355d48279994c395abcd81e8559c672
scsonic <scsonic@gmail.com>
2022-07-08 00:09:01 +0800

    Adjust layout and Night mode

--- app/src/main/java/com/ygk/kktest/MyApplication.kt
+++ app/src/main/java/com/ygk/kktest/MyApplication.kt
@@ -1,10 +1,15 @@
 package com.ygk.kktest
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MyApplication: Application() {
+    override fun onCreate() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        super.onCreate()
+    }
 }
 
 /*
